a8cfbcb6d8695c96b5422715e0af071c697ca202
cocojisoo <yjs1019a@gmail.com>
2025-06-19 17:25:10 +0900

    [Bronze I] Title: FizzBuzz, Time: 108 ms, Memory: 14212 KB -BaekjoonHub

---
+++ Java/백준/Bronze/28702. FizzBuzz/FizzBuzz.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] list = new String[3];
+		int temp = 0, num = 0;
+		
+		for (int i = 0; i < 3; i++) {
+			list[i] = br.readLine();
+			try {
+				num = Integer.parseInt(list[i]);
+				temp = i;
+			} catch (NumberFormatException e) {
+				continue;
+			}
+		}
+		num += 3 - temp;
+		
+		if (num % 15 == 0)
+			System.out.print("FizzBuzz");
+		else if(num % 3 == 0)
+			System.out.print("Fizz");
+		else if(num % 5 == 0)
+			System.out.print("Buzz");
+		else	
+			System.out.println(num);
+		
+	}
+	
+}
